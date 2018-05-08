7ebe19121aa5c12d3770158d4b84bd3f51bb503b
Collin <lefeberc@gmail.com>
2018-05-08 11:50:42 -0400

    update

--- snips/openbox_autostart.sh
+++ snips/openbox_autostart.sh
@@ -1,10 +1,24 @@
 # assuming a xubuntu base
 
-# thinkpad touchpad while typing
-syndaemon -i 0.5 -d
+(
+    xdg-mime default google-chrome.desktop x-scheme-handler/http;
+    xdg-mime default google-chrome.desktop x-scheme-handler/https;
+)
 
+# settings?
+#(xfsettingsd)&
+# for software center etc
+(/usr/lib/policykit-1-gnome/polkit-gnome-authentication-agent-1)&
+# touchpad
+(syndaemon -i 0.5 -d)&
+# volume
 (xfce-volumed)&
+# volumeicon
 (volumeicon)&
+# bg
 (nitrogen --restore)&
+# tray
 (tint2)&
+# conky
 (conky -d)&
+
