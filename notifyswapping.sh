7d1da80ce9bd79c198f9c526f045e77d14fb942d
Collin <lefeberc@gmail.com>
2018-05-14 10:52:01 -0400

    new free fmt

--- notifyswapping.sh
+++ notifyswapping.sh
@@ -1,6 +1,14 @@
 #!/usr/bin/env bash
 
 export DISPLAY=:0 # for cron
-left="$(free -m | grep '^Mem' | awk '{print $4}')"
+mem="$(free -m)"
+
+if echo "$mem" | grep -q 'available'
+then
+    left="$(free -m | grep '^Mem' | awk '{print $7}')"
+else
+    left="$(free -m | grep '^Mem' | awk '{print $4}')"
+fi
+
 #echo "MEM: $left" | logger -t notifyswapping
 [ "$left" -lt "700" ] && notify-send "Mem left: $left"
