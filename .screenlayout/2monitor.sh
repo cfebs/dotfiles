65ed54e4a7d33e3d18cdf061056c83560d6739ab
Collin Lefeber <hello@cfebs.com>
2023-06-01 09:37:03 -0400

    xrandr: new output names

--- .screenlayout/2monitor.sh
+++ .screenlayout/2monitor.sh
@@ -1,2 +1,7 @@
 #!/bin/sh
-xrandr --output DP-0 --mode 2560x1440 --pos 2560x0 --rotate left --output DP-1 --off --output DP-2 --primary --mode 2560x1440 --pos 0x0 --rotate normal --output DP-3 --off --output HDMI-0 --off --output DP-4 --off --output DP-5 --off
+
+if [ -n "$WAYLAND_DISPLAY" ]; then
+	echo "Not running xrandr, looks like we are in wayland because WAYLAND_DISPLAY is set"
+	exit 0
+fi
+xrandr --output DisplayPort-0 --primary --mode 2560x1440 --pos 0x0 --rotate normal --output DisplayPort-1 --mode 2560x1440 --pos 2560x0 --rotate left --output DisplayPort-2 --off --output HDMI-A-0 --off
